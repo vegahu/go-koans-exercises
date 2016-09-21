6f512e0ff9d75de1be854fe6ef4a9ba2afa468a5
Hugo Vega <hugotcc@gmail.com>
2016-09-21 18:52:15 +0200

    all done

---
+++ about_common_interfaces.go
@@ -0,0 +1,34 @@
+package go_koans
+
+import "bytes"
+import "io"
+
+func aboutCommonInterfaces() {
+	{
+		in := new(bytes.Buffer)
+		in.WriteString("hello world")
+
+		out := new(bytes.Buffer)
+
+		/*
+		   Your code goes here.
+		   Hint, use these resources:
+
+		   $ godoc -http=:8080
+		   $ open http://localhost:8080/pkg/io/
+		   $ open http://localhost:8080/pkg/bytes/
+		*/
+    io.Copy(out, in)
+		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
+	}
+
+	{
+		in := new(bytes.Buffer)
+		in.WriteString("hello world")
+
+		out := new(bytes.Buffer)
+		io.CopyN(out, in, 5)
+
+		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
+	}
+}
---
+++ about_panics.go
@@ -0,0 +1,14 @@
+package go_koans
+
+func divideFourBy(i int) int {
+	return 4 / i
+}
+
+const __divisor__ = 0
+
+func aboutPanics() {
+	// panics are exceptional errors at runtime
+
+	n := divideFourBy(2)
+	assert(n == 2) // panics are exceptional errors at runtime
+}
